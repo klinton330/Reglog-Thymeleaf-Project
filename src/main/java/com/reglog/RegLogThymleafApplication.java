5f8338fdf067755346b5148f6d553b5310f28c3e
HOME <klintonece@gmail.com>
2023-04-14 01:24:00 +0530

    Initial commit

--- src/main/java/com/reglog/RegLogThymleafApplication.java
+++ src/main/java/com/reglog/RegLogThymleafApplication.java
@@ -3,11 +3,14 @@ package com.reglog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 public class RegLogThymleafApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RegLogThymleafApplication.class, args);
+		System.out.println();
 	}
 
 }
